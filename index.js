46979d35f105053119ad701166f654620e790ca0
Sankaranarayanan Krishnamurthy <sankark1609@gmail.com>
2023-03-11 08:06:42 +0530

    Added to mongod atlas cloud database and made changes to package.json

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/myappDB");
+mongoose.connect("mongodb+srv://sankar1609:sankar2002@cluster0.pit9omq.mongodb.net/myappDB");
 
 const userSchema = new mongoose.Schema({
     username: String,
